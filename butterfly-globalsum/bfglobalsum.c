3f42f670fa941b97f883ad41f899b99edd1df6e6
DiegoOzuna <ozunad2002@gmail.com>
2024-02-26 17:00:20 -0800

    added print statements to butterfly global sum to show clear process.

--- butterfly-globalsum/bfglobalsum.c
+++ butterfly-globalsum/bfglobalsum.c
@@ -27,10 +27,12 @@ int main(int argc, char* argv[]){
         if (rank % (2*i) == 0){       //if process is evenly divisible
             if(rank + i < size){      //and if the process has a neighbor to add to
                 MPI_Recv(&temp, 1, MPI_INT, rank+i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //recieve the data from other process, and 
+                printf("Process %d received %d from process %d\n", rank, temp, rank+i); //print out the communication
                 local_sum += temp; //add into this process local sum
             }
         } else if ((rank - i) % (2*i) == 0){ //if process is odd/not evenly divisible, locate and send information towards the left (an even process)
             MPI_Send(&local_sum, 1, MPI_INT, rank-i, 0, MPI_COMM_WORLD);
+            printf("Process %d sent %d to process %d\n", rank, local_sum, rank-i); //print out the communication
             break; //leave the loop (even side will handle the value passed, no other purpose for odd)
         }
     }
@@ -49,9 +51,11 @@ int main(int argc, char* argv[]){
         if(rank < size && rank % (2*i) == 0){ //if we are still within bounds and are evenly divisible
             if(rank + i < size){ //making sure "neighbor" exists
                 MPI_Send(&local_sum, 1, MPI_INT, rank+i, 0, MPI_COMM_WORLD); //update neighbor with information
+                printf("Process %d informed %d\n", rank, rank+i); //print out the communication
             }
         } else if(rank < size && (rank - i) % (2*i) == 0){ //basically stating "If I am on right hand side"
             MPI_Recv(&temp, 1, MPI_INT, rank-i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //"grab info from left"
+            printf("Process %d updated by process %d \n", rank, rank-i); //print out the communication
             local_sum = temp; //"update self"
         }
     }
