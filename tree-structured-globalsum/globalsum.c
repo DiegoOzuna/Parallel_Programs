d33e8c54660af515c1774219644d310420701f65
DiegoOzuna <ozunad2002@gmail.com>
2024-02-26 16:47:21 -0800

    added print statements to tree-struc global sum to show clear process of program

--- tree-structured-globalsum/globalsum.c
+++ tree-structured-globalsum/globalsum.c
@@ -20,14 +20,18 @@ int main(int argc, char* argv[]){
     int local_sum = rank+1; //assume the local sum is rank+1
     int temp;
 
+    printf("%d starting value: %d \n",rank, local_sum);
+
     for(int i =1; i < size; i*=2){
         if (rank % (2*i) == 0){       //if process is evenly divisible
             if(rank + i < size){      //and if the process has a neighbor to add to
                 MPI_Recv(&temp, 1, MPI_INT, rank+i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //recieve the data from other process, and 
+                printf("Process %d received %d from process %d\n", rank, temp, rank+i); //print out the communication
                 local_sum += temp; //add into this process local sum
             }
         } else if ((rank - i) % (2*i) == 0){ //if process is odd/not evenly divisible, locate and send information towards the left (an even process)
             MPI_Send(&local_sum, 1, MPI_INT, rank-i, 0, MPI_COMM_WORLD);
+            printf("Process %d sent %d to process %d\n", rank, local_sum, rank-i); //print out the communication
             break; //leave the loop (even side will handle the value passed, no other purpose for odd)
         }
     }
